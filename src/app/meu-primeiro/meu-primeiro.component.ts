e2a65ccb399a619ba1decf7c4ab044c128343ca4
leandro-viana <leandrovianadc@gmail.com>
2018-11-08 11:04:13 -0300

    First Commit
    Hello World code and first Component.

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component.</p>
+    `
+})
+export class MeuPrimeiroComponent { }
